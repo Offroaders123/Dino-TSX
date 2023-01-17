ea518e8e5e00e4fc1304937d1b61b0b750d6733c
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-01-16 17:43:01 -0800

    JSX Testing
    Started working on my own implementation for this again, so I did some testing to see how texsaur currently handles some things.

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,11 @@
 import jsx from "texsaur";
 import "./text-editor.js";
 
-document.body.append(<text-editor />);
+const mySVG = <svg>
+  <use href=""/>
+</svg> as SVGElement;
+
+document.body.append(
+  <text-editor class="custom-css-class"/>,
+  mySVG
+);
